d230e7ba13fc244282c5da31932c7a0faf1294e0
Riya <whoriyashukla@gmail.com>
2024-04-18 10:22:08 +0530

    new update on files

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print("hello world")
+
+with open("riya.txt", mode='w') as file:
+    file.write("this is a new file called riya.txt\n")
+    file.write("Hello, world!")
+
+with open("riya.txt", mode='r') as file:
+    print(file.read())
