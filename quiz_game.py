4c0d44c9fa77be5b0b3715d88393fb167386e4be
Riya <whoriyashukla@gmail.com>
2024-04-21 13:18:15 +0530

    last changes

--- quiz_game.py
+++ quiz_game.py
@@ -2,40 +2,40 @@ print("Welcome to the computer game!")
 
 playing = input("Do you want to play? ")
 
-if playing == "yes":
+if playing.lower() == "yes":
     print("Okay! Let's get started!")
     count = 0
     print("First question!")
     x1 = input("What is the capital of India? ")
-    if x1 == "New Delhi":
+    if x1.lower() == "new delhi":
         print("Correct answer!!")
         count+=1
     else:
         print("Better luck next time!")
     print("Second question!")
     x2 = input("Which planet is known as the Red Planet? ")
-    if x2 == "Mars":
+    if x2.lower()  == "mars":
         print("Correct answer!!")
         count+=1
     else:
         print("Better luck next time!")
     print("Third question!")
     x3 = input("What is the largest mammal in the world? ")
-    if x3 == "Blue Whale":
+    if x3.lower()  == "blue whale":
         print("Correct answer!!")
         count+=1
     else:
         print("Better luck next time!")
     print("Fourth question!")
     x4 = input("How many continents are there in the world? ")
-    if x4 == "7":
+    if x4.lower()  == "7":
         print("Correct answer!!")
         count+=1
     else:
         print("Better luck next time!")
     print("Fifth question!")
     x5 = input("What is the largest ocean on Earth? ")
-    if x5 == "The Pacific Ocean" or "The Pacific Ocean".upper() or "The Pacific Ocean".lower():
+    if x5.lower()  == "pacific ocean":
         print("Correct answer!!")
         count+=1
     else:
